03a94d8b3bbdf45d6e19263df2eb307c8704cfa0
Alessandro <trofui@gmail.com>
2022-05-01 21:27:29 +0200

    seen SSE events

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import * as ThingDescription from './js-modules/ThingDescription.js'
 import {ajaxRequest} from './js-modules/AjaxRequests.js'
 
 const thing_id = 'com.project.thesis:greenhouse01';
+var source = new EventSource('http://localhost:8080/api/2/things/' + thing_id, { withCredentials: true });
+source.onmessage = function (event) {
+  console.log(event);
+};
 
-await ThingDescription.init(thing_id);
-console.log(ThingDescription.getFeaturesTD());
+//await ThingDescription.init(thing_id);
+//console.log(ThingDescription.getFeaturesTD());
 
 
 
